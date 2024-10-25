6a8fc09929b270e3e52c6c0e9f115d8f0c8d2ed6
Paulo Carabuena <paulo.carabuena@cit.edu>
2024-10-25 11:28:03 +0800

    Configure reverse proxy in Vite to avoid CORS issues
    - Updated `vite.config.js` to include proxy configuration for API requests.
    - Configured Axios to use the proxy path in `axiosConfig.js`.
    - Updated `App.jsx` to use the configured Axios instance for API requests.

--- campusconnect-react/src/App.jsx
+++ campusconnect-react/src/App.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import axiosInstance from "./axiosConfig.js";
 
 function App() {
     const [message, setMessage] = useState('');
 
     useEffect(() => {
-        axios.get('/print')
+        axiosInstance.get('/print')
             .then(response => {
                 setMessage(response.data);
             })
--- campusconnect-react/vite.config.js
+++ campusconnect-react/vite.config.js
@@ -1,12 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
-    server: {
-        proxy: {
-        '/print': 'http://localhost:8080'
-        }
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
     }
+  }
 })
