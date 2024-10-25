4d0b5cbdd5293be531efb98f74e620929740b852
Paulo Carabuena <45958147+pawekz@users.noreply.github.com>
2024-10-25 16:31:28 +0800

    removed files that are conflicting to the project

--- src/main/java/com/teamnullpointer/campusconnect/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.teamnullpointer.campusconnect.repository;
-import com.teamnullpointer.campusconnect.entity.UserEntity;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<UserEntity, Integer> {
-    public UserEntity findByUser(String user);
-}
