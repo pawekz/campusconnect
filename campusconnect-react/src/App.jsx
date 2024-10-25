eb4c36027025c96e1fd8e3983033e7cfe3bc5ad3
Paulo Carabuena <paulo.carabuena@cit.edu>
2024-10-25 16:02:50 +0800

    Revert "Configure reverse proxy in Vite to avoid CORS issues"
    This reverts commit 6a8fc09929b270e3e52c6c0e9f115d8f0c8d2ed6.

--- campusconnect-react/src/App.jsx
+++ campusconnect-react/src/App.jsx
@@ -1,12 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import axiosInstance from "./axiosConfig.js";
 
 function App() {
     const [message, setMessage] = useState('');
 
     useEffect(() => {
-        axiosInstance.get('/print')
+        axios.get('/print')
             .then(response => {
                 setMessage(response.data);
             })
