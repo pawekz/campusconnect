6768eb90dfb77c3ff5ab13157308b32c567dc973
Paulo Carabuena <45958147+pawekz@users.noreply.github.com>
2024-10-31 09:46:28 +0800

    modified RequestMapping

--- src/main/java/com/teamnullpointer/campusconnect/controller/AnalyticsController.java
+++ src/main/java/com/teamnullpointer/campusconnect/controller/AnalyticsController.java
@@ -8,7 +8,7 @@ import java.math.BigDecimal;
 import java.util.List;
 
 @RestController
-@RequestMapping("/analytics")
+@RequestMapping("/API/analytics")
 public class AnalyticsController {
 
     @Autowired
