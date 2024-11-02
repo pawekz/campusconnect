be2e8e890046973ac7c4602c2da745085dbe30ff
Paulo Carabuena <45958147+pawekz@users.noreply.github.com>
2024-11-02 17:56:18 +0800

    refactored `User` to `AppUser`, added `Login` and `Registration` backend

--- src/main/java/com/teamnullpointer/campusconnect/CampusconnectApplication.java
+++ src/main/java/com/teamnullpointer/campusconnect/CampusconnectApplication.java
@@ -4,8 +4,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+//purpose of exclude is to disable temporarily the security configuration of Spring Boot,
+// example is that if you send account details to the database, it will throw an error 401 Unauthorized
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class CampusconnectApplication {
     private static final Logger logger = LoggerFactory.getLogger(CampusconnectApplication.class);
 
