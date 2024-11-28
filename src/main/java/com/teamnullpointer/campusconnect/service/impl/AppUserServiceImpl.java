7e27919422d4a0f063d0bd6a06d66f3783c37703
Neil Bas <neiladrian.bas@cit.edu>
2024-11-28 13:27:01 +0800

    app user service impl broken, pushing to my branch because i forgot to block it from uploading

---
+++ src/main/java/com/teamnullpointer/campusconnect/service/impl/AppUserServiceImpl.java
@@ -0,0 +1,48 @@
+package com.teamnullpointer.campusconnect.service.impl;
+
+import com.teamnullpointer.campusconnect.DTO.AppUserDTO;
+import com.teamnullpointer.campusconnect.DTO.LoginDTO;
+import com.teamnullpointer.campusconnect.repository.AppUserRepository;
+import com.teamnullpointer.campusconnect.response.LoginResponse;
+import com.teamnullpointer.campusconnect.service.AppUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AppUserServiceImpl implements AppUserService {
+
+    @Autowired
+    private AppUserRepository appUserRepository;
+
+    @Override
+    public String addUser(AppUserDTO userDTO) {
+        // Implementation for adding user
+    }
+
+    @Override
+    public LoginResponse loginAppUser(LoginDTO loginDTO) {
+        // Implementation for logging in user
+    }
+
+    @Override
+    public Page<AppUserDTO> getAllUsersWithPagination(Pageable pageable) {
+        // Implementation for getting all users with pagination
+    }
+
+    @Override
+    public void deleteUser(Long userId) {
+        // Implementation for deleting user by Long id
+    }
+
+    @Override
+    public void deleteUser(int userId) {
+        // Implementation for deleting user by int id
+    }
+
+    @Override
+    public boolean isEmailInUse(String email) {
+        return appUserRepository.existsByEmail( (email);
+    }
+}
