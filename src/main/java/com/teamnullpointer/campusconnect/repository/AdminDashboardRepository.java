e66dc201a54cd7f0eb9bf7713946836b174ed864
Paulo Carabuena <45958147+pawekz@users.noreply.github.com>
2025-11-22 16:22:18 +0800

    refactor: rename files for consistency and clarity

--- src/main/java/com/teamnullpointer/campusconnect/repository/AdminDashboardRepository.java
+++
@@ -1,7 +0,0 @@
-package com.teamnullpointer.campusconnect.repository;
-
-import com.teamnullpointer.campusconnect.entity.AdminDashboardEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdminDashboardRepository extends JpaRepository<AdminDashboardEntity, Integer> {
-}
